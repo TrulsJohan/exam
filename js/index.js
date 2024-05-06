10ec1a2616bd9123e205a4fff0280b62491865c4
Truls Johan Jacobsen-Lamøy <truls@Trulss-MacBook-Pro.local>
2024-05-06 20:45:28 +0200

    feat: make form now works

--- js/index.js
+++ js/index.js
@@ -2,12 +2,17 @@ const newPostBtn = document.querySelector(".new-post-btn");
 const profile = document.querySelector(".profile");
 
 
-function authAccess () {
-    const accessToken = localStorage.getItem('accessToken');
-    console.log(accessToken);
-    if (accessToken){
-        newPostBtn.style.display = "block";
-        profile.style.display = "block";
+function authAccess() {
+    const authDataString = localStorage.getItem('authData');
+    if (authDataString) {
+        const authData = JSON.parse(authDataString);
+        if (authData.accessToken) {
+            newPostBtn.style.display = "block";
+            profile.style.display = "block";
+        } else {
+            newPostBtn.style.display = "none";
+            profile.style.display = "none";
+        }
     } else {
         newPostBtn.style.display = "none";
         profile.style.display = "none";
